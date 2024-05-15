75fb3405f3486380c769b58f462949330357d140
Omokhefue <mokhefue@gmail.com>
2024-05-15 15:17:16 +0100

    sidebar component

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { Flex, Box } from "@chakra-ui/react";
+
+import { Divider } from "@chakra-ui/react";
+import Profile from "./Profile";
+import AllChats from "./AllChats";
+import ArchivedChats from "./ArchivedChats";
+import Friends from "./Friends";
+import Work from "./Work";
+import Logo from "../logo/Logo";
+import Logout from "./Logout";
+import Edit from "./Edit";
+
+const Sidebar = () => {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      flexDir="column"
+      h="100%"
+      mt={3}
+    >
+      <Box>
+        <Logo size={"10px"} />
+      </Box>
+
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        flexDir="column"
+        gap={4}
+      >
+        <AllChats />
+        <ArchivedChats />
+        <Friends />
+        <Work />
+
+        <Divider mt="auto" w="60%" mx="auto" />
+
+        <Profile />
+        <Edit />
+      </Flex>
+
+      <Logout />
+    </Flex>
+  );
+};
+export default Sidebar;
