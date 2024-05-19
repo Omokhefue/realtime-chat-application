c7ef769b3c227ac84e0f41c0aad96747a5e2f1cb
Omokhefue <mokhefue@gmail.com>
2024-05-19 22:43:15 +0100

    minor changes

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -8,7 +8,7 @@ const PageLayout = () => {
       // bg={`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url('./bg.jpg')`}
       bgSize="cover"
       bgPos="center"
-        minH="100vh"
+      minH="100vh"
       maxW="full"
       centerContent
       display="flex"
